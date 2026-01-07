e2615da78fee5df32879769e91e670349ced3f5a
Lwrnsu <jaylawrencedeluna.2007@gmail.com>
2026-01-07 13:16:17 +0800

    Revisions for Day 2 Task

--- 02.py
+++ 02.py
@@ -1,14 +1,28 @@
 #January 7, 2025. 
 
-#Defined the variable to be use in while loop. 
-user_input = '';
+#It Defines the variable to be use in while loop.
+user_input = ''
 
-#Determines the output of the user. 
-while user_input != 'exit':
-    user_input = input('Enter a number: ')
-    if int(user_input) % 3 == 0 and int(user_input) % 5 == 0:
-        print('FizzBuzz')
-    elif int(user_input) % 3 == 0:
-        print('Fizz')
-    elif int(user_input) % 5 == 0:
-        print('Buzz')
+#The program itself, doesn't run if the user inputs "exit".
+while user_input.upper() != 'EXIT':
+    user_input = input('\nEnter a number: ')
+    
+    #It checks wether the user_input is a string or a number.
+    if not user_input.isdigit():
+        if user_input.upper() == "EXIT":
+            print("You've exited the program.")
+            break
+        else:
+            print("You've inputted a String!")
+    else:
+       numeric_value = int(user_input)
+       if numeric_value % 3 == 0 and numeric_value % 5 == 0:
+            print('FizzBuzz')
+       elif numeric_value % 3 == 0:
+            print('Fizz')
+       elif numeric_value % 5 == 0:
+            print('Buzz')
+       else:
+            print('The number is not divisible by 3 or 5.')
+    
+    print('If you wish to exit, enter "exit".')
